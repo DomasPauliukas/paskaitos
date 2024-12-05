38027966e38a7870d71ad89aa3ffcaa62a0ad069
DomasPauliukas <Domas0319@gmail.com>
2024-12-05 11:36:19 +0200

    first commit

---
+++ JavaScript/2024-12-05/script.js
@@ -0,0 +1,19 @@
+console.log('labas')
+console.log('')
+
+let name = 'John'
+let pavarde = "Doe"
+let miestas = `Kaunas`
+let amzius = 32
+let yraStudentas = true
+
+console.log(name, typeof name)
+console.log(amzius, typeof amzius)
+console.log(yraStudentas,typeof yraStudentas)
+
+amzius = 42
+
+console.log(amzius)
+
+console.log(name + ' ' + pavarde + ' yra is ' + miestas + '.')
+console.log(`Sveiki, ${name} ${pavarde} yra is ${miestas}.`)
