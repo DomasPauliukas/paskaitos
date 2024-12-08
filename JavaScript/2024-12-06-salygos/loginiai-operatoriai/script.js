d16733f5791787bf3b77556eb21654b25ee0e0df
DomasPauliukas <Domas0319@gmail.com>
2024-12-08 16:20:35 +0200

    loginiai operatoriai

---
+++ JavaScript/2024-12-06-salygos/loginiai-operatoriai/script.js
@@ -0,0 +1,13 @@
+// Parašykite if sąlygą, kad patikrintumėte, ar amžius yra nuo 14 iki 90 metų imtinai.
+// Reiškia, kad amžius gali būti lygus tiek 14, tiek 90 metų ir viskas tarp jų.
+
+if (age >= 14 && age <= 90)
+
+// Parašykite if sąlygą, kad patikrintumėte, ar amžius NĖRA nuo 14 iki 90 metų imtinai.
+// Parašykite du variantus: pirmąjį naudodami NOT !, antrąjį be jo.
+
+if (!(age >= 14 && age <= 90))
+
+if (age <= 14 || age >= 90)
+
+
