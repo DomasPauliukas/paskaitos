2f7d72ebf0d50b5b3f98ef9d0a6d0ff9386204a8
DomasPauliukas <Domas0319@gmail.com>
2024-12-09 19:49:39 +0200

    loops uzduotys

---
+++ JavaScript/2024-12-09-loops/script.js
@@ -0,0 +1,79 @@
+// Sukurti ciklus su skaičiais nuo 1 iki 100 ir nuo 100 iki 1. Šie ciklai:
+// Kiekvienos užduoties išvesties tekstą suformuluoti, jog būtų parašytas užduoties sprendimas, pvz.: 
+// 1 * 2 = 2
+// 2 * 2 = 4
+// 3 * 2 = 6
+// ir t.t.
+
+
+// 1. Padaugina skaičių iš 2.
+
+            // for (let i = 1 ; i <=100 ; i++){
+            //     let times2 = i * 2
+            //     console.log(`${i}*2 = ${times2}`)
+            //   }
+
+            // for (let i=100; i>0; i--){
+            //     let times2 = i * 2
+            //     console.log(`${i}*2 = ${times2}`)
+            // }
+
+// 2. Padaugina skaičių iš 5.
+
+            // for (let i = 1 ; i <=100 ; i++){
+            //     let times5 = i * 5
+            //     console.log(`${i}*5 = ${times5}`)
+            // }
+
+            // for (let i=100 ; i > 0 ; i--){
+            //     let times5 = i * 5
+            //     console.log(`${i}*5 = ${times5}`)
+            // }
+
+// 3. Prideda prie skaičiaus 5.
+
+            // for (let i = 1 ; i <=100 ; i++){
+            //     let plus5 = i + 5
+            //     console.log(`${i}+5 = ${plus5}`)
+            // }
+
+            // for (let i = 100 ; i > 0 ; i--){
+            //     let plus5 = i + 5
+            //     console.log(`${i}+5 = ${plus5}`)
+            // }
+
+// 4. Atima iš skaičiaus 2.
+
+            // for (let i = 1 ; i <=100 ; i++){
+            //     let minus2 = i - 2
+            //     console.log(`${i}-2 = ${minus2}`)
+            // }
+
+            // for (let i=100; i > 0 ; i--){
+            //     let minus2 = i - 2
+            //     console.log(`${i}-2 = ${minus2}`)
+            // }
+
+// 5. Pakelia skaičių kvadratu.
+
+            // for (let i=1 ; i <=100 ; i= i+1){
+            //     let square2 = i ** 2
+            //     console.log(`${i}*2*2 = ${square2}`)
+            // }
+
+            // for (let i = 100 ; i > 0 ; i--){
+            //     let square2 = i**2
+            //     console.log(`${i}*2*2 = ${square2}`)
+            // }
+
+// 6. Pakelia skaičių kūbu.
+
+            // for (let i=1 ; i<=100 ; i+=1){
+            //     let cube = i ** 3
+            //     console.log(`${i}*3*3 = ${cube}`)
+            // }
+
+            // for (let i = 100 ; i > 0 ; i--){
+            //     let cube = i**3
+            //     console.log(`${i}*3*3 = ${cube}`)
+            // }
