6d49a7fa0431f0c48490e056ddcc3b0672b0e7a1
DomasPauliukas <Domas0319@gmail.com>
2024-12-17 00:56:15 +0200

    funkcijos-praktika

--- JavaScript/2024-12-13-CA-funkcijos/script1.js
+++ JavaScript/2024-12-13-CA-funkcijos/script1.js
@@ -155,3 +155,142 @@ let randomNumber = () => Math.floor(Math.random() * 11)
 let randomSquare = num => Math.pow(num, 2)
 
 console.log(randomSquare(randomNumber()))
+
+
+
+
+// m/s į km/s 
+
+// Parašykite funkciją convertMStoKMH, kuri konvertuoja greitį iš metrų per sekundę į kilometrus per valandą.
+
+let convertMStoKMH = meters => meters * 3.6
+console.log(convertMStoKMH())
+
+
+// Parašykite funkciją compareNumber, kuri palygina du skaičius ir grąžina didesnįjį iš jų. Jei skaičiai yra lygūs, funkcija turėtų grąžinti "lygūs". Įsitikinkite, kad abu argumentai yra skaičiai; jei ne, grąžinkite klaidos pranešimą.
+
+// Rezultatas
+
+// console.log(compareNumber(4, 7)); // 7
+// console.log(compareNumber(5, 5)); // "equal"
+// console.log(compareNumber("5", 5)); // "Error: most arguments must be numbers"
+
+function compareNumber(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+      return 'error: it has to be numbers'
+    }
+    if (x > y) {
+      return x
+    }
+    if (x < y) {
+      return y
+    }
+    return 'skaiciai lygus'
+  }
+  console.log(compareNumber(5, 5))
+
+//   const compareNumber = (a, b) => {
+//     if (typeof a !== 'number' || typeof b !== 'number') {
+//         return "Error: most arguments must be numbers";
+//     }
+//     if (a === b) {
+//         return "equal";
+//     }
+//     return a > b ? a : b;
+// };
+
+
+// Parašykite funkciją isLeapYear, kuri priima year ir pasako ar metai yra keliamieji. Funkcija turi grąžinti true, jei metai keliamieji, ir false priešingu atveju.
+
+const isLeapYear = (year) => (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+console.log(isLeapYear(2032))
+
+
+// Parašykite funkciją findMaxInArray, kuri ras ir grąžins didžiausią elementą masyve. Galime daryti prielaidą, kad visi array elementai yra skaičiai.
+
+// console.log(findMaxInArray([1, 3, 2, 5, 4])); // 5
+// console.log(findMaxInArray([-1, -3, -2, -5, -4])); // -1
+
+
+function findMaxInArray(arr) {
+    let max = arr[0]
+    for (let i = 1; i <= arr.length; i++) {
+      if (arr[i] > max) {
+        max = arr[i]
+      }
+    }
+    return max
+  }
+  
+  console.log(findMaxInArray([1, 3, 2, 5, 4])); 
+
+
+
+//   Parašykite funkciją sumArray, kuri suskaičiuoja ir grąžina visų elementų sumą masyve. Kaip ir užduotyje prieš tai, galime daryti prielaidą, kad visi array elementai yra skaičiai.
+
+// console.log(sumArray([1, 2, 3, 4, 5])); // 15
+
+function sumArray(arr) {
+    let arraySum = 0
+    for (let i = 0; i < arr.length; i++) {
+      arraySum += arr[i]
+    }
+    return arraySum
+  }
+  
+  console.log(sumArray([1, 2, 3, 4, 5]))
+
+
+//   Sukurkite funkciją arMasyvaiLygūs, kuri palygina du masyvus ir grąžina true, jei jie yra identiški, kitu atveju - false.
+
+// console.log(areArraysEqual([1, 2, 3], [1, 2, 3])); // true
+// console.log(areArraysEqual([1, 2, 3], [1, 2, 4])); // false
+
+function arMasyvaiLygūs (arr1, arr2){
+    for (let i = 0; i < arr1.length; i++){
+        if (arr1[i] !== arr2[i]){
+        return false
+      }
+    }
+    return true
+  }
+
+console.log(areArraysEqual([1, 2, 3], [1, 2, 3])); // true
+console.log(areArraysEqual([1, 2, 3], [1, 2, 4])); // false
+
+
+
+// Eilutės apvertimas
+
+// Sukurkite funkciją reverseString, kuri apverčia eilutę be jokių išorinių funkcijų ar metodų.
+
+// Rezultatas
+
+// console.log(reverseString("Labas")); // "sabaL"
+
+
+function reversedWord (word){
+    let result = ''
+    for (let i = word.length - 1 ; i >= 0 ; i--){
+        result += word[i]
+    }
+    return result
+}
+console.log(reversedWord('Kompiuteris'))
+
+
+
+// function reverseString(changeWord) {
+//     let reversedWord = changeWord.split('')
+//     reversedWord.reverse()
+//     let result = reversedWord.join('')
+//     return result
+//   }
+  
+//   console.log(reverseString("Labas")); // "sabaL"
+
+//   function reverseString(changeWord) {
+//     return changeWord.split('').reverse().join('')
+//   }
+  
+//   console.log(reverseString("labas")); // "sabaL"
