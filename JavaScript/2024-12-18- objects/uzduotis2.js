82d6eb5e50b4de02fd1763318f6589fc40032f4f
DomasPauliukas <Domas0319@gmail.com>
2024-12-19 14:22:58 +0200

    tasks done

---
+++ JavaScript/2024-12-18- objects/uzduotis2.js
@@ -0,0 +1,430 @@
+const posts = [
+    {
+      id: 1,
+      title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      id: 2,
+      title: "qui est esse",
+      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      id: 3,
+      title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+      id: 4,
+      title: "eum et est occaecati",
+      body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+      id: 5,
+      title: "nesciunt quas odio",
+      body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+    {
+      id: 6,
+      title: "dolorem eum magni eos aperiam quia",
+      body: "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    },
+    {
+      id: 7,
+      title: "magnam facilis autem",
+      body: "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+    },
+    {
+      id: 8,
+      title: "dolorem dolore est ipsam",
+      body: "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+    },
+    {
+      id: 9,
+      title: "nesciunt iure omnis dolorem tempora et accusantium",
+      body: "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+    },
+    {
+      id: 10,
+      title: "optio molestias id quia eum",
+      body: "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+    },
+    {
+      id: 11,
+      title: "et ea vero quia laudantium autem",
+      body: "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+    },
+    {
+      id: 12,
+      title: "in quibusdam tempore odit est dolorem",
+      body: "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+    },
+    {
+      id: 13,
+      title: "dolorum ut in voluptas mollitia et saepe quo animi",
+      body: "aut dicta possimus sint mollitia voluptas commodi quo doloremque\niste corrupti reiciendis voluptatem eius rerum\nsit cumque quod eligendi laborum minima\nperferendis recusandae assumenda consectetur porro architecto ipsum ipsam"
+    },
+    {
+      id: 14,
+      title: "voluptatem eligendi optio",
+      body: "fuga et accusamus dolorum perferendis illo voluptas\nnon doloremque neque facere\nad qui dolorum molestiae beatae\nsed aut voluptas totam sit illum"
+    },
+    {
+      id: 15,
+      title: "eveniet quod temporibus",
+      body: "reprehenderit quos placeat\nvelit minima officia dolores impedit repudiandae molestiae nam\nvoluptas recusandae quis delectus\nofficiis harum fugiat vitae"
+    },
+    {
+      id: 16,
+      title: "sint suscipit perspiciatis velit dolorum rerum ipsa laboriosam odio",
+      body: "suscipit nam nisi quo aperiam aut\nasperiores eos fugit maiores voluptatibus quia\nvoluptatem quis ullam qui in alias quia est\nconsequatur magni mollitia accusamus ea nisi voluptate dicta"
+    },
+    {
+      id: 17,
+      title: "fugit voluptas sed molestias voluptatem provident",
+      body: "eos voluptas et aut odit natus earum\naspernatur fuga molestiae ullam\ndeserunt ratione qui eos\nqui nihil ratione nemo velit ut aut id quo"
+    },
+    {
+      id: 18,
+      title: "voluptate et itaque vero tempora molestiae",
+      body: "eveniet quo quis\nlaborum totam consequatur non dolor\nut et est repudiandae\nest voluptatem vel debitis et magnam"
+    },
+    {
+      id: 19,
+      title: "adipisci placeat illum aut reiciendis qui",
+      body: "illum quis cupiditate provident sit magnam\nea sed aut omnis\nveniam maiores ullam consequatur atque\nadipisci quo iste expedita sit quos voluptas"
+    },
+    {
+      id: 20,
+      title: "doloribus ad provident suscipit at",
+      body: "qui consequuntur ducimus possimus quisquam amet similique\nsuscipit porro ipsam amet\neos veritatis officiis exercitationem vel fugit aut necessitatibus totam\nomnis rerum consequatur expedita quidem cumque explicabo"
+    },
+    {
+      id: 21,
+      title: "asperiores ea ipsam voluptatibus modi minima quia sint",
+      body: "repellat aliquid praesentium dolorem quo\nsed totam minus non itaque\nnihil labore molestiae sunt dolor eveniet hic recusandae veniam\ntempora et tenetur expedita sunt"
+    },
+    {
+      id: 22,
+      title: "dolor sint quo a velit explicabo quia nam",
+      body: "eos qui et ipsum ipsam suscipit aut\nsed omnis non odio\nexpedita earum mollitia molestiae aut atque rem suscipit\nnam impedit esse"
+    },
+    {
+      id: 23,
+      title: "maxime id vitae nihil numquam",
+      body: "veritatis unde neque eligendi\nquae quod architecto quo neque vitae\nest illo sit tempora doloremque fugit quod\net et vel beatae sequi ullam sed tenetur perspiciatis"
+    },
+    {
+      id: 24,
+      title: "autem hic labore sunt dolores incidunt",
+      body: "enim et ex nulla\nomnis voluptas quia qui\nvoluptatem consequatur numquam aliquam sunt\ntotam recusandae id dignissimos aut sed asperiores deserunt"
+    },
+    {
+      id: 25,
+      title: "rem alias distinctio quo quis",
+      body: "ullam consequatur ut\nomnis quis sit vel consequuntur\nipsa eligendi ipsum molestiae et omnis error nostrum\nmolestiae illo tempore quia et distinctio"
+    }]
+
+//     7. Naudojant posts masyvą, atlikti žemiau nurodytas užduotis ir atsakymus išvesti į konsolę. Visoms užduotis kurti funkciją ir jai, kaip parametrą, paduoti nurodytą masyvą.
+
+
+// 7.1. Gauti pirmo masyvo nario reikšmę.
+
+let function71 = (element) => {
+    return element[0]
+  }
+  
+  console.log(function71(posts))
+
+
+// 7.2. Gauti paskutinio masyvo nario reikšmę.
+
+let function72 = function (element){
+    return element.at(-1)
+}
+console.log(function72(posts))
+
+
+// 7.3. Gauti 15 masyvo nario reikšmę.
+
+let function73 = function (element){
+    return element.at(14)
+}
+console.log(function73(posts))
+
+
+// 7.4. Gauti 13 masyvo nario reikšmę skaičiuojant nuo galo.
+
+let function74 = function (element){
+    return element.at(-13)
+}
+console.log(function74(posts))
+
+// 7.5. Gauti vidurinio masyvo nario reikšmę.
+
+let function75 = function (element){
+    return element[Math.floor(element.length / 2)]
+}
+console.log(function75(posts))
+
+
+// 7.6. Gauti pirmų keturių masyvo narių reikšmes.
+
+let function76 = function (element) {
+    return element.slice(0,4)
+  }
+  console.log(function76(posts))
+
+
+// 7.7. Gauti paskutinių penkių masyvo narių reikšmes.
+
+let function77 = function (element) {
+    return element.slice(-5)
+  }
+  console.log(function77(posts))
+
+
+// 7.8. Gauti nuo penkto iki penkiolikto masyvo narių reikšmes.
+
+let function78 = (element) => element.slice(4,15)
+
+  console.log(function78(posts))
+
+
+// 7.9. Gauti kas antro masyvo nario reikšmes.
+
+let function79 = function (element) {
+    let result = []
+    for (let i = 0 ; i < element.length ; i++){
+      if (i % 2 === 0){
+        result.push(element[i])
+      }
+    }
+    return result
+  }
+  console.log(function79(posts))
+  
+
+// 7.10. Gauti pirmo ir paskutinio masyvo narių reikšmes.
+
+
+let function710 = function (element) {
+    console.log(element.at(0))
+    console.log(element.at(-1))
+  }
+  console.log(function710(posts))
+
+
+// 7.11. Gauti pirmą masyvo narį ir į konsolę išvesti jo property "title" reikšmę.
+
+let function712 = function (element) {
+    return element.at(0).title
+  }
+  console.log(function712(posts))
+  
+
+// 7.12. Gauti antrą masyvo narį ir į konsolę išvesti jo property "body" reikšmę.
+
+let function712 = function (element) {
+    return element.at(1).body
+  }
+  console.log(function712(posts))
+
+
+// 7.13. Gauti trečią masyvo narį ir į konsolę išvesti jo property "title" ir "body" reikšmes tokiu formatu: Post title is: 'post title' and the content is: 'post content'.
+
+let function713 = function (element) {
+    console.log(`Post title is: "` + element.at(2).title + `" and the content is: "` + element.at(2).body + `"`)
+  }
+  function713(posts)
+
+
+// 7.14. Išvesti visus masyvo narius į konsolę.
+
+let function714 = function (element) {
+    element.forEach(num => console.log(num))
+  }
+  function714(posts)
+
+
+//   7.15. Išvesti visų masyvo narių property "title" į konsolę.
+
+let function715 = function (element) {
+    element.forEach(num => console.log(`*` + num.title))
+  }
+  console.log(function715(posts))
+
+
+// 2. kitas budas ---- 
+
+// let function715 = function (element) {
+//     element.forEach(function (num)  {
+//       return console.log(`*` + num.title)
+//   })
+//   }
+//   console.log(function715(posts))
+
+
+// 7.16. Išvesti visų masyvo narių property "body" į konsolę.
+
+
+let function716 = function (element){
+    element.forEach( num => console.log(`*` + num.body))
+}
+function716(posts)
+
+
+// 7.17. Išvesti visų masyvo narių property "title" ir "body" į konsolę tokiu formatu: Title: "post title". Content: "post content".
+
+let function717 = function (element) {
+    element.forEach(num => 
+      console.log(`TITLE: "` + num.title + `". CONTENT: "` + num.body + `".`)
+    )
+  }
+  function717(posts)
+
+
+// 7.18. Išvesti pirmų keturių masyvo narių property "title" ir "body" į konsolę tokiu formatu: Title: "post title". Content: "post content".
+
+let function718 = function (element) {
+    let newElement = element.slice(0, 4)
+    newElement.forEach(num => console.log(`Title: "${num.title}". Content: "${num.body}".
+  `))
+  }
+  console.log(function718(posts))
+
+
+// 7.19. Išvesti paskutinių septynių masyvo narių property "title" ir "body" į konsolę tokiu formatu: Title: "post title". Content: "post content".
+
+let function719 = function (element) {
+    let newElement = element.slice(-7)
+    console.log(newElement)
+    newElement.forEach(num => {
+      console.log(`Title: "` + num.title + `". Content: "` + num.body + `".`)
+    })
+  }
+  function719(posts)
+
+
+// 7.22. Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas prasideda raide "s".
+
+
+let function722 = function (element) {
+    element.filter(num => {
+      if (num.title[0] === 's'){
+      console.log(num)
+    }
+    }
+    )}
+  console.log(function722(posts))
+
+// 7.23. Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas baigiasi "t" arba "m".
+
+let function723 = function (element){
+    element.filter(function(num){
+        if(num.title.at(-1) === 't' || num.title.at(-1) === 'm'){
+            console.log(num)
+        }
+    })
+}
+function723(posts)
+
+
+// 7.24. Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas turi daugiau simbolių nei 15.
+
+let function724 = function (element) {
+    element.filter(function(num){
+        if(num.title.length > 15){
+            console.log(num)
+        }
+    })
+}
+function724(posts)
+
+
+// Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas turi mažiau simbolių nei 20 ir kurių "body" turi daugiau simbolių nei 50.
+
+let function725 = function (element){
+element.forEach((num) => {
+    if(num.title.length < 20 && num.body.length > 50){
+        console.log(num)
+    }
+})
+}
+function725(posts)
+
+
+// 7.26. Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas turi tarp 20 ir 30 simbolių ir kurių "body" turi tarp 70 ir 130 simbolių.
+
+
+let function726 = function (element){
+    element.forEach((num) => {
+        if((num.title.length > 20 && num.title.length < 30) && (num.body.length > 70 && num.body.length < 130)){
+            console.log(num)
+        }
+    })
+    }
+    function726(posts)
+
+
+// 7.27. Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas turi simbolių junginį "it", o "body" turi simbolių junginį "quo".
+
+let function727 = function (element){
+    element.forEach((num) => {
+        if((num.title.includes('it')) && (num.body.includes('quo'))){
+            console.log(num)
+        }
+    })
+    }
+    function727(posts)
+
+
+7.28. Į konsolę išvesti tik tuos masyvo narius, kurių "body" turi žodį "sit". Jeigu žodis turi šį simbolių junginį, tačiau tai nėra pilna jo reikšmė (pvz. žodžiai "sitto" ar "quasit"), jie neturėtų būti atvaizduojami.
+
+
+let function728 = function (element) {
+    element.forEach(num => {
+      // console.log(num.body)
+      let splitBody = num.body.split(' ')
+      // console.log(splitBody)
+    for (let i = 0 ; i < splitBody.length ; i++){
+      if (splitBody[i] === 'sit'){
+        console.log(num)
+      }
+    }
+    })
+  }
+  function728(posts)
+
+// 7.29. Į konsolę išvesti tik tuos masyvo narius, kurių pavadinimas turi daugiau nei 3 "a" raides, o "body" turi mažiau nei 7 "o" raides.
+
+
+let function729 = function (element) {
+    element.forEach((num) => {
+      let titleCount = 0
+      let bodyCount = 0
+      let splitTitle = num.title.split('')
+      // console.log(splitTitle)
+      let splitBody = num.body.split('')
+      // console.log(splitBody)
+      for (let i = 0; i < splitTitle.length; i++) {
+        if (splitTitle[i] === 'a') {
+          titleCount++
+        }
+      }
+          // console.log(titleCount)
+      for (let j = 0 ; j < splitBody.length ; j++){
+        if (splitBody[j] === 'o'){
+          bodyCount++
+        }
+      }
+      // console.log(bodyCount)
+      if (titleCount > 3 && bodyCount < 7){
+        console.log(num)
+      }
+    }
+    )
+  }
+  
+  function729(posts)
+
