4a4680a7fae986241fd4044e3d345a9ed8d3a0ba
DomasPauliukas <Domas0319@gmail.com>
2024-12-19 17:44:12 +0200

    CA finshed

--- JavaScript/2024-12-16-CA-array/arrays-SORT-REDUCE.js
+++ JavaScript/2024-12-16-CA-array/arrays-SORT-REDUCE.js
@@ -64,3 +64,64 @@ const people = [
       age: 16
     }
   ];
+
+// 1.  Prafiltruokite masyvą, kad rodytų tik pilnamečius. 
+
+let function1 = function (item) {
+  item.forEach(num => {
+    if (num.age >= 18){
+      console.log(num)
+    }
+  })
+}
+function1(people)
+
+// Pakoreguokite aukščiau nurodytą pratimą, kad pilnamečių array rodytų tik vardus (amžius jau nesvarbu). Rezultatas turi būti: ["Petras", "Antanas", "Diana"].
+
+let function2 = function (item) {
+  let nameArray = []
+item.forEach(num => {
+  if (num.age >= 18) {
+    nameArray.push(num.name)
+  }
+})
+return nameArray
+}
+console.log(function2(people))
+
+
+// Antro pratimo array išrikiuokite A-Z tvarka. Optimizuokite kodą, kad visas veiksmas tilptų į vieną eilutę.
+
+let function3 = function (item) {
+  let nameArray = []
+item.forEach(num => {
+  if (num.age >= 18) {
+    nameArray.push(num.name)
+  }
+})
+return nameArray.sort()
+}
+console.log(function3(people))
+
+
+// Sukurkite funkciją. Ji priims vieną argumentą – array su objektais, kurie turės du parametrus:
+
+// name 
+
+// ir price. 
+
+// Grąžinkite – brangiausią ir pigiausią prekes. 
+
+// Turi veikti padavus neribotą kiekį elementų (pavyzdyje apačioje – tik du).
+
+// Pvz:
+
+// iškvietimas: fn([{name: "lemonade", price: 50}, {name: "lime", price: 10}])
+
+// grąžins: {brangiausias: "lemonade", pigausias: "lime"}
+
+let function4 = function (array) {
+  array.sort((a,b) => a.price - b.price)
+  return console.log(`Brangiausias: ` + array.at(-1).name + `, pigiausias: ` + array[0].name)
+}
+function4([{ name: "lemonade", price: 50 }, { name: "lime", price: 10 }, {name: "strawberry", price: 65}])
