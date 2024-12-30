8b51dafce25f91589ac3655497268eff08920608
DomasPauliukas <Domas0319@gmail.com>
2024-12-30 15:51:43 +0200

    testimonials-finished

--- JavaScript/2024-12-27-DOM/testimonials.js
+++ JavaScript/2024-12-27-DOM/testimonials.js
@@ -1,240 +1,148 @@
-let content = document.querySelector('#content')
-
-let outerBox = document.createElement('div')
-outerBox.classList.add('outer-box')
-content.append(outerBox)
-
-let mainBox = document.createElement('div')
-mainBox.classList.add('main-box')
-outerBox.append(mainBox)
-
-let innerBox1 = document.createElement('div')
-innerBox1.classList.add('inner-box')
-mainBox.append(innerBox1)
-
-
-
-let item1 = document.createElement('div')
-item1.classList.add('item')
-innerBox1.append(item1)
-
-let header = document.createElement('header')
-item1.append(header)
-
-let imgWrapper = document.createElement('img')
-imgWrapper.src = "images/image-daniel.jpg"
-header.append(imgWrapper)
-
-let initials = document.createElement('div')
-initials.classList.add('initials')
-header.append(initials)
-
-let span1 = document.createElement('span')
-span1.textContent = 'Daniel Clifford'
-initials.append(span1)
-
-let span2 = document.createElement('span')
-span2.textContent = 'Verified Graduate'
-initials.append(span2)
-
-let mainItem = document.createElement('main')
-item1.append(mainItem)
-
-let section1 = document.createElement('section')
-mainItem.append(section1)
-
-let section1Title = document.createElement('h2')
-section1Title.textContent = 'I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.'
-section1.append(section1Title)
-
-let section2 = document.createElement('section')
-mainItem.append(section2)
-
-let section2Paragraph = document.createElement('p')
-section2Paragraph.textContent = '“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”'
-section2.append(section2Paragraph)
-
-
-
-
-
-let item2 = document.createElement('div')
-item2.classList.add('item')
-innerBox1.append(item2)
-
-let header2 = document.createElement('header')
-item2.append(header2)
-
-let imgWrapper2 = document.createElement('img')
-imgWrapper2.src = "images/image-jonathan.jpg"
-header2.append(imgWrapper2)
-
-let initials2 = document.createElement('div')
-initials2.classList.add('initials')
-header2.append(initials2)
-
-let span3 = document.createElement('span')
-span3.textContent = 'Jonathan Walters'
-initials2.append(span3)
-
-let span4 = document.createElement('span')
-span4.textContent = 'Verified Graduate'
-initials2.append(span4)
-
-let mainItem2 = document.createElement('main')
-item2.append(mainItem2)
-
-let section3 = document.createElement('section')
-mainItem2.append(section3)
-
-let section1Title2 = document.createElement('h2')
-section1Title2.textContent = 'The team was very supportive and kept me motivated'
-section3.append(section1Title2)
-
-let section4 = document.createElement('section')
-mainItem2.append(section4)
-
-let section2Paragraph2 = document.createElement('p')
-section2Paragraph2.textContent = '“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. ”'
-section4.append(section2Paragraph2)
-
-
-
-
-
-
-let item3 = document.createElement('div')
-item3.classList.add('item')
-innerBox1.append(item3)
-
-let header3 = document.createElement('header')
-item3.append(header3)
-
-let imgWrapper3 = document.createElement('img')
-imgWrapper3.src = "images/image-jeanette.jpg"
-header3.append(imgWrapper3)
-
-let initials3 = document.createElement('div')
-initials3.classList.add('initials')
-header3.append(initials3)
-
-let span5 = document.createElement('span')
-span5.textContent = 'Jeanette Harmon'
-initials3.append(span5)
-
-let span6 = document.createElement('span')
-span6.textContent = 'Verified Graduate'
-initials3.append(span6)
-
-let mainItem3 = document.createElement('main')
-item3.append(mainItem3)
-
-let section5 = document.createElement('section')
-mainItem3.append(section5)
-
-let section1Title3 = document.createElement('h2')
-section1Title3.classList.add('dark-text')
-section1Title3.textContent = 'An overall wonderful and rewarding experience'
-section5.append(section1Title3)
-
-let section6 = document.createElement('section')
-mainItem3.append(section6)
-
-let section2Paragraph3 = document.createElement('p')
-section2Paragraph3.textContent = '“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”'
-section6.append(section2Paragraph3)
-
-
-
-
-
-
-
-let item4 = document.createElement('div')
-item4.classList.add('item')
-innerBox1.append(item4)
-
-let header4 = document.createElement('header')
-item4.append(header4)
-
-let imgWrapper4 = document.createElement('img')
-imgWrapper4.src = "images/image-patrick.jpg"
-header4.append(imgWrapper4)
-
-let initials4 = document.createElement('div')
-initials4.classList.add('initials')
-header4.append(initials4)
-
-let span7 = document.createElement('span')
-span7.textContent = 'Patrick Abrams'
-initials4.append(span7)
-
-let span8 = document.createElement('span')
-span8.textContent = 'Verified Graduate'
-initials4.append(span8)
-
-let mainItem4 = document.createElement('main')
-item4.append(mainItem4)
-
-let section7 = document.createElement('section')
-mainItem4.append(section7)
-
-let section1Title4 = document.createElement('h2')
-section1Title4.textContent = 'Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.'
-section7.append(section1Title4)
-
-let section8 = document.createElement('section')
-mainItem4.append(section8)
-
-let section2Paragraph4 = document.createElement('p')
-section2Paragraph4.textContent = '“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”'
-section8.append(section2Paragraph4)
-
-
-
-
-let innerBox2 = document.createElement('div')
-innerBox2.classList.add('inner-box')
-mainBox.append(innerBox2)
-
-
-let item5 = document.createElement('div')
-item5.classList.add('item')
-innerBox2.append(item5)
-
-let header5 = document.createElement('header')
-item5.append(header5)
-
-let imgWrapper5 = document.createElement('img')
-imgWrapper5.src = "images/image-kira.jpg"
-header5.append(imgWrapper5)
-
-let initials5 = document.createElement('div')
-initials5.classList.add('initials')
-header5.append(initials5)
-
-let span9 = document.createElement('span')
-span9.textContent = 'Kira Whittle'
-initials5.append(span9)
-
-let span10 = document.createElement('span')
-span10.textContent = 'Verified Graduate'
-initials5.append(span10)
-
-let mainItem5 = document.createElement('main')
-item5.append(mainItem5)
-
-let section9 = document.createElement('section')
-mainItem5.append(section9)
-
-let section1Title5 = document.createElement('h2')
-section1Title5.classList.add('dark-text')
-section1Title5.textContent = 'Such a life-changing experience. Highly recommended!'
-section9.append(section1Title5)
-
-let section10 = document.createElement('section')
-mainItem5.append(section10)
-
-let section2Paragraph5 = document.createElement('p')
-section2Paragraph5.textContent = '“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”'
-section10.append(section2Paragraph5)
+let mainTestimonialsData = [
+    {   
+        name: 'Daniel Clifford',
+        title: 'I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.',
+        paragraph: '“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the best - and most grueling - time of my life. Since completing the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”',
+        imageSrc: 'images/image-daniel.jpg',
+        verified: false
+    },
+    {   
+        name: 'Jonathan Walters',
+        title: 'The team was very supportive and kept me motivated',
+        paragraph: '“ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer for a big company. This was one of the best investments I’ve made in myself. ”',
+        imageSrc: "images/image-jonathan.jpg",
+        verified: true
+    },
+    {
+        name: 'Jeanette Harmon',
+        title: 'An overall wonderful and rewarding experience',
+        paragraph: '“ Thank you for the wonderful experience! I now have a job I really enjoy, and make a good living while doing something I love. ”',
+        imageSrc: "images/image-jeanette.jpg",
+        verified: true
+    },
+    {
+        name: 'Patrick Abrams',
+        title: 'Awesome teaching support from TAs who did the bootcamp themselves. Getting guidance from them and learning from their experiences was easy.',
+        paragraph: '“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”',
+        imageSrc: "images/image-patrick.jpg",
+        verified: true
+    }
+]
+
+let secondaryTestimonialsData = [
+    {
+        name: 'Kira Whittle',
+        title: 'Such a life-changing experience. Highly recommended!',
+        paragraph: '“ The staff seem genuinely concerned about my progress which I find really refreshing. The program gave me the confidence necessary to be able to go out in the world and present myself as a capable junior developer. The standard is above the rest. You will get the personal attention you need from an incredible community of smart and amazing people. ”',
+        imageSrc: "images/image-kira.jpg",
+        verified: true
+    }
+]
+
+function init () {
+
+    let content = document.querySelector('#content')
+    
+    let outerBox = createElement('outer-box')
+    content.append(outerBox)
+    
+    let mainBox = createElement('main-box')
+    outerBox.append(mainBox)
+    
+    let innerBox1 = createMainTestimonialsComponent()
+    mainBox.append(innerBox1)
+    
+    let innerBox2 = createSecondaryTestimonialsComponent()
+    mainBox.append(innerBox2)
+}
+
+init ()
+
+function createMainTestimonialsComponent () {
+    let innerBox1 = createElement('inner-box')
+
+    mainTestimonialsData.forEach(testimonialData => {
+    let testimonialItem = createTestimonialItem(testimonialData)
+    innerBox1.append(testimonialItem)
+    })
+
+    return innerBox1
+}
+
+function createSecondaryTestimonialsComponent () {
+    let innerBox2 = createElement('inner-box')
+
+    secondaryTestimonialsData.forEach(testimonialData => {
+    let testimonialItem = createTestimonialItem(testimonialData)
+    innerBox2.append(testimonialItem)
+    })
+
+    return innerBox2
+}
+
+function createTestimonialItem (data) {
+
+    let {name, title, paragraph, imageSrc, verified} = data
+
+    if (!name || !paragraph){
+        return ''
+    }
+
+    let item = createElement('item')
+    
+    let header = createElement('', '', 'header')
+    item.append(header)
+    
+    if (imageSrc){
+    
+    let imgWrapper = document.createElement('img')
+    imgWrapper.src = imageSrc
+    header.append(imgWrapper)
+    }
+    
+    let initials = createElement('initials')
+    header.append(initials)
+    
+    let span1 = createElement('', name, 'span')
+    initials.append(span1)
+    
+    let verifiedText = verified ? 'Verified Graduate' : 'Not Verified'
+    let span2 = createElement('', verifiedText, 'span')
+    initials.append(span2)
+    
+    let mainItem = document.createElement('main')
+    item.append(mainItem)
+    
+    if (title){
+
+    let section1 = document.createElement('section')
+    mainItem.append(section1)
+    
+    let section1Title = createElement('', title, 'h2')
+    section1.append(section1Title)
+    }
+    
+    if (paragraph) {
+        
+    let section2 = document.createElement('section')
+    mainItem.append(section2)
+    
+    let section2Paragraph = createElement('', paragraph, 'p')
+    section2.append(section2Paragraph)
+    }
+
+    return item
+}
+
+function createElement (className, text, elementType = 'div') {
+    let element = document.createElement(elementType)
+
+    if (className){
+        element.classList.add(className)
+    }
+    if (text){
+        element.textContent = text
+    }
+
+    return element
+}
