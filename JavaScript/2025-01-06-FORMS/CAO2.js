9db7f12ac2bacb7f0d0c178ab00197f85cc7213b
DomasPauliukas <Domas0319@gmail.com>
2025-01-07 21:53:46 +0200

    2/2 done

---
+++ JavaScript/2025-01-06-FORMS/CAO2.js
@@ -0,0 +1,31 @@
+// Šauktiniai
+// vidutinis
+
+// Kai kuriose šalyse reikia atlikti privalomąją karinę tarnybą, jei esate tarp 18 ir 30 metų, ir, jei neturite kriminalinio įrašo. Sukurkite programą, kuri su input + radio button pasakytų ar tam žmogui gali reikėti eiti į atlikti tarnybą ar ne (h1 elemente). 
+
+// Užuomina
+
+// Pasirinkite su querySelector tik pažymėtą radio input (yra toks CSS selektorius), ir perskaitykite jo reikšmę.
+
+const formArmy = document.querySelector('#formArmy')
+const criminalInput = document.querySelector('[name="person-criminal"]')
+
+const formTitle = document.createElement('h1')
+formTitle.textContent = 'Pasitikrinkite ar galite būti šauktinių sąrašę'
+formArmy.before(formTitle)
+
+const resultTitle = document.createElement('h2')
+formArmy.after(resultTitle)
+
+formArmy.addEventListener('submit', (event)=> {
+    event.preventDefault()
+
+    const isCriminal = criminalInput.checked
+
+    if (age.value >= 18 && age.value <= 30 && !isCriminal){
+        resultTitle.textContent = 'Pasitikrinkite! Jūs galite būti šauktinių sąraše.'
+    } else {
+        resultTitle.textContent = 'Jūs neatitinkate kriterijų, kad galėtumėte būti šautkinių sąraše.'
+    }
+formArmy.reset()
+})
