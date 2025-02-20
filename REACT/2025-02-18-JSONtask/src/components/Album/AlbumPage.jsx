6ab1ad624d5f9f27b2730aeed8a4526e803f6d1c
DomasPauliukas <Domas0319@gmail.com>
2025-02-20 18:35:53 +0200

    setIsLoading + error added

--- REACT/2025-02-18-JSONtask/src/components/Album/AlbumPage.jsx
+++ REACT/2025-02-18-JSONtask/src/components/Album/AlbumPage.jsx
@@ -1,39 +1,54 @@
-import { useState } from "react"
-import { useEffect } from "react"
-import { Link } from "react-router-dom"
-import { API_URL } from "../APIaddress"
-import HomeNav from "../HomeNav"
+import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { API_URL } from "../APIaddress";
+import HomeNav from "../HomeNav";
 
-function AlbumPage () {
+function AlbumPage() {
+  const [albums, setAlbums] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
 
-const [albums, setAlbums] = useState([])
-
-useEffect(() => {
-    async function fetchAlbums () {
-        const res = await fetch(`${API_URL}/albums?_embed=photos`)
-        const albumsData = await res.json()
-        setAlbums(albumsData)
+  useEffect(() => {
+    async function fetchAlbums() {
+      try {
+        const res = await fetch(`${API_URL}/albums?_embed=photos`);
+        const albumsData = await res.json();
+        setAlbums(albumsData);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setError("Something went wrong...");
+      }
     }
-    fetchAlbums()
-})
+    fetchAlbums();
+  });
 
-    return (
-        <div>
-            <button>
-                <Link to={`/Albums/Create`}>Create Album</Link>
-            </button>
-            <div>Album Page</div>
-            {albums.map((album, index) => (
-                <li key={index}>
-                    <Link to={`/Albums/${album.id}`}>{album.id}. {album.title} ({album.photos.length} photos)</Link>
-                </li>
+  if (error) {
+    return <p>{error}</p>;
+  }
 
-            ))}
-            
+  if (isLoading) {
+    return <p>Data is loading</p>;
+  }
 
-            <HomeNav/>
-        </div>
-    )
+  return (
+    <div>
+      <button>
+        <Link to={`/Albums/Create`}>Create Album</Link>
+      </button>
+      <div>Album Page</div>
+      {albums.map((album, index) => (
+        <li key={index}>
+          <Link to={`/Albums/${album.id}`}>
+            {album.id}. {album.title} ({album.photos.length} photos)
+          </Link>
+        </li>
+      ))}
+
+      <HomeNav />
+    </div>
+  );
 }
 
-export default AlbumPage
+export default AlbumPage;
